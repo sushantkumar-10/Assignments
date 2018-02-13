0400f18a4fa05474b07d178d67202c9dcfecdf61
Sushant Kumar (BAS) <SU397361@wipro.com>
2018-02-13 11:57:29 +0530

    first commit

---
+++ src/main/java/Advance_selenium/com/Opencart_Advance/App.java
@@ -0,0 +1,13 @@
+package Advance_selenium.com.Opencart_Advance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Advance_selenium/com/Opencart_Advance/test1.java
@@ -0,0 +1,15 @@
+package Advance_selenium.com.Opencart_Advance;
+
+import org.testng.annotations.Test;
+
+public class test1 {
+	
+
+	@Test
+	public void Test1(){
+		
+		System.out.println("this is test 1");
+		
+	}
+
+}
